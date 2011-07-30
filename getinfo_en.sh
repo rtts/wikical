8a3cc76d5afd585eebb06c4cd26b3fab042d9689
jj <jj@r2src.com>
2011-07-30 23:12:50 +0200

    hand-edited all days... phew

--- getinfo_en.sh
+++ getinfo_en.sh
@@ -3,7 +3,7 @@
 # This program calls wikiscraper with each day of the year as its argument,
 # and saves the output. This is how all the files got in this directory.
 
-for m in January February March April May June July August October November December
+for m in September # January February March April May June July August October November December
 do
 	for d in {1..31}
 	do
